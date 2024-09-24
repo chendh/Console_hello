8968fb2332025e22ef436f00184cb18b3f5319cc
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-09-24 10:53:49 +0800

    簡化命名空間並改進乘法表輸出格式
    將 `std::cout` 和 `std::cin` 替換為 `cout` 和 `cin`，並在文件開頭新增 `using namespace std;`。修改了乘法表的輸出格式，從單純輸出乘積改為輸出乘法運算式，例如 `i * j = i * j`。將 `std::endl` 替換為 `endl`。

--- Console_hello/Console_hello.cpp
+++ Console_hello/Console_hello.cpp
@@ -1,19 +1,20 @@
 ﻿// main.cpp
 #include <iostream>
+using namespace std;
 
 void printMultiplicationTable(int n) {
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= n; ++j) {
-            std::cout << i * j << "\t";
+            cout << i << "*" << j << "=" << i * j << "\t";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 }
 
 int main() {
     int n = 1;
-    std::cout << "請輸入乘法表的大小: ";
-    std::cin >> n;
+    cout << "請輸入乘法表的大小: ";
+    cin >> n;
 
     printMultiplicationTable(n);
 
